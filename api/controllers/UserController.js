ded3e9688412a0361e949b4dd11d54450af361a5
Jason Ng <ywng@ust.hk>
2014-05-03 12:04:22 +0800

    default: no special comment

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -23,10 +23,28 @@ module.exports = {
    *    `/user/create`
    */
    create: function (req, res) {
-    
-    // Send a JSON response
-    return res.json({
-      hello: 'world'
+    var params = req.params.all();
+    User.create(params, function(err, user){
+      if (err){ res.send(500, err); }else{
+        if(sails.config.user.requireUserActivation){
+          var emailTemplate = res.render('email/email.ejs', {user: user}, function(err, list){  
+
+            nodemailer.send({
+              name:       user.firstName + ' ' + user.lastName,
+              from:       sails.config.nodemailer.from,
+              to:         user.email,
+              subject:       'New Account Acivation Required',
+              messageHtml: list
+            }, function(err, response){
+              sails.log.debug('nodemailer sent', err, response);
+            });
+            res.send(200, user);
+
+          });
+        }else{
+          res.send(200, user);
+        }
+      }
     });
   },
 
